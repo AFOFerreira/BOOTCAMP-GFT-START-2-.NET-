916226dcc6245261fd34cce0e99883e3889fe4b5
Cod3rN3rd <desenvolvimento.abnfel@outlook.com>
2021-06-05 11:44:19 -0300

    Inicio da aplicação
    Aplicação criada com o código básico.

---
+++ Revisao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
