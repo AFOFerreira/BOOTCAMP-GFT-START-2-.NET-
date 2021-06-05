05577129bbb4b93612a0dc56f6388dbf8bc22c3e
Cod3rN3rd <desenvolvimento.abnfel@outlook.com>
2021-06-05 13:02:14 -0300

    Aplicação completa
    Sistema simples de cadastro de alunos, com listagem de notas e media geral.

--- Revisao/Program.cs
+++ Revisao/Program.cs
@@ -6,7 +6,104 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Aluno[] alunos = new Aluno[5];
+            int indiceAluno = 0;
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //TODO: adicionar aluno
+                        Console.WriteLine("Informe o nome do aluno:");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+                        Console.WriteLine("Informe a nota do aluno:");
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("O valor da nota deve ser decimal");
+                        }
+                        if (indiceAluno < 5)
+                        {
+                            alunos[indiceAluno] = aluno;
+                            indiceAluno++;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("nao e possivel adicionar o aluno, limite atingido");
+                        }
+                        break;
+                    case "2":
+                        //TODO: listar alunos
+                        foreach (var obj in alunos)
+                        {
+                            if (!string.IsNullOrEmpty(obj.Nome))
+                                Console.WriteLine($"Aluno: {obj.Nome} - NOTA: {obj.Nota}");
+                        }
+                        break;
+                    case "3":
+                        //TODO: calcular media geral
+                        decimal notaTotal = 0;
+                        var nrAlunos = 0;
+                        for (int i = 0; i < alunos.Length; i++)
+                        {
+                            if (!string.IsNullOrEmpty(alunos[i].Nome))
+                            {
+                                notaTotal = notaTotal + alunos[i].Nota;
+                                nrAlunos++;
+                            }
+                        }
+                        var mediaGeral = notaTotal / nrAlunos;
+                        Conceito conceitoGeral;
+                        if (mediaGeral < 2)
+                        {
+                            conceitoGeral = Conceito.E;
+                        }
+                        else if (mediaGeral < 4)
+                        {
+                            conceitoGeral = Conceito.D;
+                        }
+                        else if (mediaGeral < 6)
+                        {
+                            conceitoGeral = Conceito.C;
+                        }
+                        else if (mediaGeral < 8)
+                        {
+                            conceitoGeral = Conceito.B;
+                        }
+                        else
+                        {
+                            conceitoGeral = Conceito.A;
+                        }
+
+                        Console.WriteLine($"MEDIA GERAL: {mediaGeral} - CONCEITO GERAL: {conceitoGeral}");
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Informe a opcao desejada: ");
+            Console.WriteLine("1- Inserir aluno");
+            Console.WriteLine("2- Listar alunos");
+            Console.WriteLine("3- Calcular media geral");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine("Opcao escolhida: " + opcaoUsuario);
+            Console.WriteLine();
+            return opcaoUsuario;
         }
     }
 }
